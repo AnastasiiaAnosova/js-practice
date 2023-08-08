fe523d5c59683959b23f052a1147089e119adeec
AnastasiiaAnosova <118612211+AnastasiiaAnosova@users.noreply.github.com>
2023-08-08 12:15:00 +0000

    Deploying to gh-pages from @ AnastasiiaAnosova/js-practice@28c80b992db8440b2ca9b8c990149ccde7ed1d24 🚀

---
+++ index.6353962c.js
@@ -0,0 +1,2 @@
+document.querySelector(".login-form").addEventListener("input",(function(n){const{name:t,value:o}=n.target;e[t]=o,console.log(e)}));let e={};
+//# sourceMappingURL=index.6353962c.js.map
---
+++ index.71c3dd70.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector(".login-form").addEventListener("input",(function(n){var o=n.target,t=o.name,r=o.value;e[t]=r,console.log(e)}));var e={}}();
+//# sourceMappingURL=index.71c3dd70.js.map
