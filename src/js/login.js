3f20c9f42f11206c03c2ec8803b7e36d0dd95ab9
AnastasiiaAnosova <118612211+AnastasiiaAnosova@users.noreply.github.com>
2023-08-08 18:29:49 +0300

    added coment

--- src/js/login.js
+++ src/js/login.js
@@ -18,32 +18,40 @@ const USER_DATA = {
   email: "user@mail.com",
   password: "secret",
 };
-const LOCAL_KEY = 'login-data';
-let userData = {};
+const LOCAL_KEY = 'login-data';//ключ по якому буде зберігатися наші дані в local storage   /the key by which our data will be stored in local storage
+let userData = {};//обʼєкт, де ми будемо зберігати дані,які ввів нащ користувач, щоб увійти в систему    /the object where we will store the data entered by our user to log in
 const formElement = document.querySelector('.login-form');
-const loginBtn = document.querySelector('.login-btn');
-const inputs = document.querySelectorAll('.login-input');
-const saveData = storage.load('LOCAL_KEY');
+const inputs = document.querySelectorAll('.login-input');submitBtn = document.querySelector('.login-btn');
+const saveData = storage.load('LOCAL_KEY');//отримуємо дані з local storage,які ввів користувач   /get data from local storage that the user has entered
 const todoForm = document.querySelector('.todo')
 
 formElement.addEventListener('input', onSaveData);
-formElement.addEventListener('submit', onCheckData);
+formElement.addEventListener('submit', onSubmitForm);
 
+ /*робимо перевірку отриманих даних з localStorage
+ якщо там є дані, тобто true, тоді змінюємо текст кнопки на 'Logout' та встановлюємо атрибут для полів input 'readonly',щоб вони були недоступні для введення або зміни даних
+ /check the received data from localStorage
+ if there is data there, that is true, then change the text of the button to 'Logout' and set the attribute for the input fields to 'readonly' so that they are not available for entering or changing data
+ */
 if (saveData) {
-    loginBtn.textContent = 'Logout';
     inputs.forEach(input => input.setAttribute('readonly', true));
+    submitBtn.textContent = 'Logout';
 }
 
+/* ми використовуємо деструктуризацію об'єкта для отримання двох властивостей (name та value) з об'єкта event.target
+й ці дані зберігаємо в оголошених змінних name, value
+/we use object destructuring to get two properties (name and value) from the event.target object
+object and store this data in the declared variables name, value
+ */
 function onSaveData(event) {
     const { name, value } = event.target;
-    userData[name] = value;
-    // console.log(userData);
+    userData[name] = value;//записуємо дані в обʼєкт для зберігання даних,що ввів користувач    /write the data to the data storage object entered by the user
 }
 
-function onCheckData(e) {
-    e.preventDefault();
-    if (loginBtn.textContent === 'Logout') {
-        loginBtn.textContent = 'Login';
+function onSubmitForm(e) {
+    e.preventDefault();//відміняємо дії за змовчування при submit     /cancel default actions on submit
+    if (submitBtn.textContent === 'Logout') {
+        submitBtn.textContent = 'Login';
         inputs.forEach(input => input.removeAttribute('readonly'));
         storage.remove(LOCAL_KEY);
         userData = {};
@@ -58,7 +66,7 @@ function onCheckData(e) {
         return Notify.failure('The entered data does not match');
     }
     storage.save(LOCAL_KEY, userData);
-    loginBtn.textContent = 'Logout';
+    submitBtn.textContent = 'Logout';
     inputs.forEach(input => input.setAttribute('readonly', true));
     todoForm.style.display = 'flex';
     formElement.reset();
